3f7f168b1788b288c733db8bca439b9c13d240e7
Adam Cowley <adam@adamcowley.co.uk>
2023-05-10 12:53:30 +0100

    Update AWS credentials in workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  // base: '/developer',
+  base: '/developer',
   build: {
     assets: 'assets',
   }
